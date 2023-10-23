6283d042efad71ba8dc5847d6950d6289c0ce34b
leslie pacheco <lesli_sharai_pacheco@hotmail.com>
2023-10-23 08:37:42 -0600

    Login y registros con estilos y funcional

--- src/lib/fireBase.js
+++ src/lib/fireBase.js
@@ -7,18 +7,25 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyABq8MSX--dAspZWbMB59umYaMVjLHkNjI",
-  authDomain: "gastrotour-17ada.firebaseapp.com",
-  projectId: "gastrotour-17ada",
-  storageBucket: "gastrotour-17ada.appspot.com",
-  messagingSenderId: "38325177476",
-  appId: "1:38325177476:web:43195b46f4f4c5ed2c06d1",
-  measurementId: "G-9BKFFCLQBP"
-};
 
+  apiKey: "AIzaSyABq8MSX--dAspZWbMB59umYaMVjLHkNjI",
+
+  authDomain: "gastrotour-17ada.firebaseapp.com",
+
+  projectId: "gastrotour-17ada",
+
+  storageBucket: "gastrotour-17ada.appspot.com",
+
+  messagingSenderId: "38325177476",
+
+  appId: "1:38325177476:web:43195b46f4f4c5ed2c06d1",
+
+  measurementId: "G-9BKFFCLQBP"
+
+};
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-console.log('***', app);
+console.log('****', app);
 // const db = getFirestore(app);
 
 // // Get a list of cities from your database
