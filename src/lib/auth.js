ba104846de40b72fdb5f7ef401e663a1dec2b73e
Kotte91 <kottevaldebenito@gmail.com>
2023-11-07 10:36:17 -0300

    login se encuentra terminado

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,4 +1,5 @@
-import { createUserWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithPopup, signInWithEmailAndPassword, GoogleAuthProvider } from 'firebase/auth';
+
 import { auth } from './fireBase.js';
 
 export const registerNewUser = (email, password) => new Promise((resolve, reject) => {
@@ -17,7 +18,39 @@ export const registerNewUser = (email, password) => new Promise((resolve, reject
   });
 });
 
+export const loginUser = (email, password) => new Promise((resolve, reject) => {
+  signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+    const user = userCredential.user;
+    resolve(user.email);
+  }).catch((error) => {
+    const errorCode = error.code;
+    if (errorCode === 'auth/invalid-login-credentials') {
+      const errorMessage = error.message;
+      reject(new Error('Usuario y/o Contraseña invalidas'));
+    }
+  });
+});
 export const registerGoogle = (provider) => (
 
   signInWithPopup(auth, provider)
 );
+
+// export const loginGoogleGoogle = (provider) => (
+
+//   signInWithPopup(auth, provider)
+// );
+
+export const loginGoogle = (provider) => (
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // const credential = GoogleAuthProvider.credentialFromResult(result);
+      // const token = credential.accessToken;
+      const user = result.user;
+      return user;
+    }).catch((error) => {
+      const errorCode = error.code;
+      return errorCode;
+      // const errorMessage = error.message;
+      // const email = error.customData.email;
+      // const credential = GoogleAuthProvider.credentialFromError(error);
+    }));
--- src/templates/login.js
+++ src/templates/login.js
@@ -1,14 +1,15 @@
 import { GoogleAuthProvider } from 'firebase/auth';
-import { registerGoogle } from '../lib/auth';
+import { loginGoogle, loginUser } from '../lib/auth';
 
 function login(navigateTo) {
-// <------------------------- Creación de elementos HTML --------------------------------->
+  // <------------------------- Creación de elementos HTML --------------------------------->
   const section = document.createElement('section');
   const title = document.createElement('h2');
   const buttonReturn = document.createElement('button');
   const form = document.createElement('form');
   const inputEmail = document.createElement('input');
   const inputPass = document.createElement('input');
+  const alerts = document.createElement('span');
   const buttonLogin = document.createElement('input');
   const buttonGoogle = document.createElement('button');
   const imgGoogle = document.createElement('img');
@@ -20,27 +21,48 @@ function login(navigateTo) {
   // <------------------- Campo para ingresar correo electrónico --------------------------->
   inputEmail.placeholder = 'Ingresa tu correo electrónico';
   inputEmail.className = 'form-data';
+  inputEmail.setAttribute('id', 'idEmail');
   // <------------------------ Campo para ingresar contraseña ------------------------------>
   inputPass.placeholder = 'Ingresa tu contraseña';
   inputPass.type = 'password';
   inputPass.className = 'form-data';
+  inputPass.setAttribute('id', 'inputPass');
+  alerts.setAttribute('id', 'alerts-error');
   // <-------------------------------- Sección de botones ---------------------------------->
   buttons.className = 'botones';
   // <------------- Botón para iniciar sesión con correo electrónico ----------------------->
   buttonLogin.value = 'Iniciar';
   buttonLogin.type = 'submit';
   buttonLogin.className = 'register';
-  // <-------------- Botón para iniciar sesión con cuenta de Google ------------------------>
-  buttonGoogle.className = 'registergoogle';
-  buttonGoogle.addEventListener('click', () => {
-    const provider = new GoogleAuthProvider();
-    registerGoogle(provider);
+  buttonLogin.setAttribute('id', 'buttonLogin');
+  buttonLogin.addEventListener('click', (e) => {
+    e.preventDefault();
+    loginUser(inputEmail.value, inputPass.value)
+      .then((ok) => {
+        if (ok) {
+          navigateTo('/muro');
+        }
+      }).catch((error) => {
+        document.getElementById('alerts-error').textContent = error;
+      });
   });
-  imgGoogle.src = '../img/001-google.png';
-  imgGoogle.alt = 'Logo Javascript';
-  imgGoogle.className = 'imgGoogle';
-  textButtonGoogle.textContent = 'Iniciar con google';
+  // <-------------- Botón para iniciar sesión con cuenta de Google ------------------------>
+  buttonGoogle.className = 'register';
+  buttonGoogle.setAttribute('id', 'buttonRegisterGoogle');
+  textButtonGoogle.textContent = 'Registrarse con google';
   textButtonGoogle.className = 'title-google';
+
+  buttonGoogle.addEventListener('click', (e) => {
+    e.preventDefault();
+    const provider = new GoogleAuthProvider();
+    const resultado = loginGoogle(provider);
+    resultado.then((user) => {
+      navigateTo('/muro');
+    }).catch((errorCode) => {
+      console.log("errorPrueba", errorCode);
+    });
+  });
+
   // <-------------------- Botón para regresar a la página "home" -------------------------->
   buttonReturn.textContent = 'Regresar al inicio';
   buttonReturn.className = 'register';
@@ -49,9 +71,9 @@ function login(navigateTo) {
   });
   // <---------------- Orden de la estructura de los elementos HTML ------------------------>
   buttonGoogle.append(imgGoogle, textButtonGoogle);
-  form.append(inputEmail, inputPass);
+  form.append(inputEmail, inputPass, alerts);
   buttons.append(buttonLogin, buttonGoogle, buttonReturn);
-  section.append(title, form, buttons);
+  section.append(title, form, buttons, buttonGoogle);
 
   return section;
 }
